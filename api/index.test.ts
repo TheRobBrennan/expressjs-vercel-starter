bccd4d51cb59c98f0859435890451be6513bc5e5
Rob Brennan <rob@therobbrennan.com>
2024-03-18 00:08:43 -0700

    Delete unnecessary API test

--- api/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import reExportedServer from './index';
-import originalServer from '../src/server';
-
-describe('API Index Exports', () => {
-  it('should re-export the same server instance as src/server', () => {
-    expect(reExportedServer).toBe(originalServer);
-  });
-});
