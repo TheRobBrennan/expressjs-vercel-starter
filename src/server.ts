81246c77b0e21786bb1f59fc47194e15f8c5c238
Rob Brennan <rob@therobbrennan.com>
2024-03-17 23:19:39 -0700

    Remove unnecessary reference code and simplify server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
 import 'dotenv/config';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-// import path from 'path';
-// import helmet from 'helmet';
 
-// import TTS from './routes/tts';
-// import File from './routes/file';
-// import Files from './routes/files';
+import Ping from './routes/ping';
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,32 +11,16 @@ const server = express();
 
 server.use(morgan('dev'));
 server.use(cors());
-// server.use(
-//   helmet({
-//     contentSecurityPolicy: {
-//       directives: {
-//         defaultSrc: ["'self'"],
-//         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-//         styleSrc: ["'self'", 'fonts.googleapis.com'],
-//       },
-//     },
-//   })
-// );
-
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
 // FUTURE: Serve static files and other assets if necessary
 // server.use(express.static(path.join(__dirname, '../public')));
 
-server.get('/', (_: Request, res: Response) => {
-  res.json({ pong: "Hello, World!", timestamp: new Date().toISOString() });
-});
+// Routes
+server.use('/', Ping);
 
-// server.use('/api/v1/files', Files);
-// server.use('/api/v1/file', File);
-// server.use('/api/v1/tts', TTS);
-
+// Start the server
 server.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
